17cc7620d326f43aa5fd6fa38fcdf594bb008fc3
JhonChica <jhon.chica@samtel.co>
2024-03-25 18:10:34 -0500

    cmabios para grpc

--- src/main/java/com/mibanco/clientecdtdigital/es/services/impl/ClienteCDTDigitalImpl.java
+++ src/main/java/com/mibanco/clientecdtdigital/es/services/impl/ClienteCDTDigitalImpl.java
@@ -15,6 +15,7 @@ import jakarta.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 @Slf4j
 @GrpcService
 public class ClienteCDTDigitalImpl implements ClienteCDTDigitalService {
